cdb5a655e00855c71bcfaca86e957a4abc7ac74d
Yisroel Allen <keystonebusinessconsulting@gmail.com>
2016-01-12 21:58:15 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+
+public class User 
+{
+	public User()
+	{
+		
+	}
+	public void runMessage()
+	{
+		Scanner crypticMessage= new Scanner(System.in);
+		String alien;
+		alienCalculator a=new alienCalculator();
+		
+		System.out.println("Enter the alien number");
+		alien=crypticMessage.next();
+		System.out.print("You have ");
+		a.calculate(alien);
+		System.out.println(" seconds to live");
+		crypticMessage.close();
+	}
+}
 
